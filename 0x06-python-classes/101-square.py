21d21f4e34972c17c73185b8465c1f68ef615d6d
Calvince Ndemo <calyndemo16@gmail.com>
2024-04-04 00:59:08 +0300

    fixed bugges

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -21,7 +21,7 @@ class Square:
     @size.setter
     def size(self, value):
         self.__size = value
-        if (type(value) != int):
+        if (type(value) is not int):
             raise TypeError("size must be an integer")
         if (value < 0):
             raise ValueError("size must be >= 0")
@@ -33,11 +33,11 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
-        if (len(value) != 2 or type(value) is not tuple):
+        if (len(value) is not 2 or type(value) is not tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         if (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if (type(value[0]) != int or type(value[1]) != int):
+        if (type(value[0]) is not int or type(value[1]) != int):
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
