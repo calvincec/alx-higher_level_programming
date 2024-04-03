15ce6e9ac95b001eaa8dcd2c08d0eb934e9628b6
Calvince Ndemo <calyndemo16@gmail.com>
2024-04-04 01:00:44 +0300

    fixed bugge2s

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -37,7 +37,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if (type(value[0]) is not int or type(value[1]) != int):
+        if (type(value[0]) is not int or type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
@@ -46,7 +46,7 @@ class Square:
 
     def my_print(self):
         """print the square depending of the size received"""
-        if (self.__size == 0):
+        if (self.__size is 0):
             print()
         else:
             if (self.__position[1] > 0):
