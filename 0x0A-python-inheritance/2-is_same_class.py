fd93587a570cfc8a2899982447388af15bc47299
Calvince Ndemo <calyndemo16@gmail.com>
2024-04-16 23:41:38 +0300

    corrected rrors

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ def is_same_class(obj, a_class):
     """
     Return true if the object is an instance of a class
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
         """
         integer_validator method
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
